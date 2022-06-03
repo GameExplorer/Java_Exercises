486b7c64663893c686bd1c4032e1ba9bd9d524ba
GameExplorer <GameExplorer@users.noreply.github.com>
2022-06-03 22:28:05 +0200

    added DN01
    added DN01

---
+++ src/Domace_Naloge/DN01.java
@@ -0,0 +1,20 @@
+package Domace_Naloge;
+
+
+public class DN01 {
+
+    // Metoda izpise besedilo s podatki o studentu in izjavo
+    // o samostojnem resevanju nalog
+    public static void main(String[] args) {
+        System.out.println("Moje ime je: Micka Kovaceva");
+        System.out.println("Moja vpisna stevilka: 63000000");
+        System.out.println();
+        System.out.println("*******************************************************");
+        System.out.println("*|*  Izjavljam,  da sem prebral/a vsebino  strani   *|*");
+        System.out.println("*|*  'Samostojna izdelava programov' na eUcilnici.  *|*");
+        System.out.println("*|*  Vse obveznosti predmeta Programiranje 2 bom    *|*");
+        System.out.println("*|*  skladno s temi navodili opravil/a samostojno.  *|*");
+        System.out.println("*******************************************************");
+    }
+
+}

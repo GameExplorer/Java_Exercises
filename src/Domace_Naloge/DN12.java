b5f74770c79c98c7e95315e04354b0a9b4eab8b7
GameExplorer <GameExplorer@users.noreply.github.com>
2022-06-08 21:37:59 +0200

    DN12

--- src/Domace_Naloge/DN12.java
+++ src/Domace_Naloge/DN12.java
@@ -1,4 +1,33 @@
 package Domace_Naloge;
 
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+
 public class DN12 {
+
+    public static void main(String[] args) {
+        try {
+            DataInputStream dis = new DataInputStream(new FileInputStream(new File(args[0])));
+            byte[] buf = new byte[8];
+
+            dis.read(buf); // header
+
+            if (buf[1] != 'P' || buf[2] != 'N' || buf[3] != 'G') {
+                System.out.println("?");
+                return;
+            }
+
+            dis.read(buf); // header IHDR chunka
+
+            int width = dis.readInt();
+            int height = dis.readInt();
+
+            System.out.printf("%d x %d\n", width, height);
+        } catch (Exception e) {
+            System.out.println("Napaka: " + e);
+        }
+    }
 }

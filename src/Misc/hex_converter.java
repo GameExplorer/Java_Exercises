9ce33392c06cc9fa26fe908cdff6b31cc8605d1d
GameExplorer <GameExplorer@users.noreply.github.com>
2022-06-12 16:53:04 +0200

    misc

--- src/Misc/hex_converter.java
+++ src/Misc/hex_converter.java
@@ -13,7 +13,7 @@ public class hex_converter {
         char[] hex ={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
         Scanner in = new Scanner(System.in);
-10
+
         System.out.print("Input a decimal number: ");
         dec_num = in.nextInt();
 

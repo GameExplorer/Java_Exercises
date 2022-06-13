86b7340b60028c568046982e08b154ecd6a3f196
GameExplorer <GameExplorer@users.noreply.github.com>
2022-06-13 20:08:18 +0200

    Update DN06.java

--- src/Domace_Naloge/DN06.java
+++ src/Domace_Naloge/DN06.java
@@ -34,56 +34,61 @@ Ker morate poiskati prvi niz (ki je soroden podanemu nizu), svetujem, da začnet
 ki vsebuje toliko a-jev kot je dolžina niza, ki je podan v argumentu) in ga povečujete. Napišete metodo
 String povecaj(String niz), ki prejme niz in vrne prvi večji niz. Primer: metoda povecaj("aaaab") vrne "aaaac".
  */
+
 public class DN06 {
 
     // Kontrolna vsota BSD (povzeto po https://en.wikipedia.org/wiki/BSD_checksum)
     static int bsdChecksum(String niz) {
-        int checksum = 0;
-
-        for (int i = 0; i<niz.length(); i++) {
-            checksum = (checksum >> 1) + ((checksum & 1) << 15);
-            checksum += niz.charAt(i);
-            checksum &= 0xffff;       /* Keep it within bounds. */
-        }
-        return checksum;
+      int checksum = 0;
+  
+      for (int i = 0; i<niz.length(); i++) {
+        checksum = (checksum >> 1) + ((checksum & 1) << 15);
+        checksum += niz.charAt(i);
+        checksum &= 0xffff;       /* Keep it within bounds. */
+      }
+      return checksum;
     }
+  
     // Motoda prejme niz in vrne prvi po abecedi večji ni
     static String povecaj(String niz) {
-        int z = niz.length()-1;
-
-        // poiščem prvo črko od zadaj, ki je različna od z ...
-        while (z >= 0) {
-            // ... če taka črka obstaja, jo povečam, vse črke za njo pa postavim na 'a';
-            if (niz.charAt(z) < 'z')
-                return niz.substring(0,z) + ((char)(niz.charAt(z)+1)) + "a".repeat(niz.length()-z-1);
-            else
-                z=z-1;
-        }
-        // ... če pa taka črka ne obstaja, večjega niza te dolžine ni, zato vrnem null
-        return null;
+      int z = niz.length()-1;
+  
+      // poiščem prvo črko od zadaj, ki je različna od z ...
+      while (z >= 0) {
+        // ... če taka črka obstaja, jo povečam, vse črke za njo pa postavim na 'a';
+        if (niz.charAt(z) < 'z')
+          return niz.substring(0,z) + ((char)(niz.charAt(z)+1)) + "a".repeat(niz.length()-z-1); 
+        else
+          z=z-1;
+      }
+      // ... če pa taka črka ne obstaja, večjega niza te dolžine ni, zato vrnem null
+      return null;
     }
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Program potrebuje en argument!");
-            System.exit(0);
+  
+    public static void main(String[] args) {        
+      if (args.length != 1) {
+        System.out.println("Program potrebuje en argument!");
+        System.exit(0);
+      }
+  
+      String niz = args[0];
+      int csNiz = bsdChecksum(niz);
+  
+  
+      // začnem z nizom, ki je enako dolg kot 1. argument in je sestavljen iz samih 'a'-jev
+      String trNiz="a".repeat(niz.length());
+  
+      while (trNiz != null) {
+        // izracunam BSD trenutnega niza ...
+        int csTr=bsdChecksum(trNiz);
+        // ... in ga primerjam z BSD 1. argumenta; če sta enaka, niz izpišem ...
+        if (csTr == csNiz) {
+          System.out.println(trNiz);
+          System.exit(0);
         }
-
-        String niz = args[0];
-        int csNiz = bsdChecksum(niz);
-
-        // začnem z nizom, ki je enako dolg kot 1. argument in je sestavljen iz samih 'a'-jev
-        String trNiz="a".repeat(niz.length());
-
-        while (trNiz != null) {
-            // izracunam BSD trenutnega niza ...
-            int csTr=bsdChecksum(trNiz);
-            // ... in ga primerjam z BSD 1. argumenta; če sta enaka, niz izpišem ...
-            if (csTr == csNiz) {
-                System.out.println(trNiz);
-                System.exit(0);
-            }
-            // ... sicer niz "povecam" in nadaljujem z iskanjem
-            trNiz = povecaj(trNiz);
-        }
+        // ... sicer niz "povecam" in nadaljujem z iskanjem
+        trNiz = povecaj(trNiz);
+      }
     }
+  
 }

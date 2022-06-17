ee3023ff2581a05f506754af1fe886d5a1c6b56d
GameExplorer <GameExplorer@users.noreply.github.com>
2022-06-17 21:48:15 +0200

    Update Naloga21.java

--- src/Izpiti/Poletni2022/Naloga21.java
+++ src/Izpiti/Poletni2022/Naloga21.java
@@ -27,6 +27,14 @@ public class Naloga21 {
         String niz = String.join(" ", args);
         izpisi(niz);
     }
-
-
 }
+
+/*
+V razredu Naloga21 napiši metodo izpisi(), ki besede danega besedila opremi s številkami (v oklepaju zapiše dolžino besede)
+in tako opremljeno besedilo izpiše na zaslon.  Primer: ob klicu izpisi("Java je super") naj metoda izpiše  Java(4) je(2)
+super(5) Metoda izpisi() naj upošteva, da ločila (presledek, vejica in pika) ne sodijo k besedi in zato ne prispevajo k njeni dolžini.
+Ob klicu   izpisi("Bravo, zelo dobro.") je pravilen izpis tak:  Bravo(5), zelo(4) dobro(5). Izhodno besedilo naj bo oblikovano tako,
+da bodo v eni izpisani vrstici največ 4 besede. Primer: Ob klicu   izpisi("Prva, druga, tretja, cetrta, peta, sesta.") je pravilen izpis tak:
+Prva(4), druga(5), tretja(6), cetrta(6), peta(4), sesta(5). Metodo izpisi() napiši v razred  Naloga21, v katerem naj bo tudi main()
+metoda  public static void main(String[] args) {     izpisi(args[0]); }
+ */
